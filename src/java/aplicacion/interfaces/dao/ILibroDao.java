e319241d18e2ea41d875addb2d34706781b52195
Diego <Diego@Diego-PC>
2019-04-29 20:45:32 -0300

    reeemplace patron DAO

--- src/java/aplicacion/interfaces/dao/ILibroDao.java
+++ src/java/aplicacion/interfaces/dao/ILibroDao.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aplicacion.interfaces.dao;
+
+import aplicacion.modelo.dominio.Libro;
+import java.util.List;
 
 /**
  *
- * @author Leo
+ * @author GRUPO ALFA
  */
 public interface ILibroDao {
     
+    void crearLibro(Libro libro);
+    List<Libro> obtenerLibros();
+    void modificarLibro(Libro libro, String codigo);
+    void eliminarLibro(Libro libro);
+    Libro buscarLibro(String codigo); 
+    void ordenarLibroAscendente();
+    void ordenarLibroDescendente();
+    List<Libro> buscarLibroporLetra(char letra);
 }
